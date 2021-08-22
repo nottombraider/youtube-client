d6f7fef08e70bece51cee5fba49bbf6fea545349
nottombraider <larysa.yavorska@outlook.com>
2021-08-22 14:25:42 +0200

    init: clean branch

--- src/app/components/thumbnail-statistics/thumbnail-statistics.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-thumbnail-statistics',
-  templateUrl: './thumbnail-statistics.component.html',
-  styleUrls: ['./thumbnail-statistics.component.scss'],
-})
-export class ThumbnailStatisticsComponent {}
