2a2f2c5a821c335b0c89a1c49e63be49062de684
nottombraider <larysa.yavorska@outlook.com>
2021-08-22 13:12:16 +0200

    feat: add login info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { LoginInfoComponent } from './components/login-info/login-info.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchBarComponent],
+  declarations: [AppComponent, HeaderComponent, SearchBarComponent, LoginInfoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
