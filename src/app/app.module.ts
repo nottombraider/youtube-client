abea1bd035e03074729b6a4c5d2ab479d41e5515
nottombraider <larysa.yavorska@outlook.com>
2021-08-22 13:14:43 +0200

    feat: add search results component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { LoginInfoComponent } from './components/login-info/login-info.component';
 import { FilteringBarComponent } from './components/filtering-bar/filtering-bar.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchBarComponent, LoginInfoComponent, FilteringBarComponent],
+  declarations: [AppComponent, HeaderComponent, SearchBarComponent, LoginInfoComponent, FilteringBarComponent, SearchResultsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
