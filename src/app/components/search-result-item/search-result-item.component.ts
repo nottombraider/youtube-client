b017e451c9d02621a78e32675ece1da1575e2462
nottombraider <larysa.yavorska@outlook.com>
2021-08-22 14:05:59 +0200

    feat: add declaration of necessary fields

--- src/app/components/search-result-item/search-result-item.component.ts
+++ src/app/components/search-result-item/search-result-item.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchResult } from '../../types/SearchResult';
 
 @Component({
   selector: 'app-search-result-item',
   templateUrl: './search-result-item.component.html',
-  styleUrls: ['./search-result-item.component.scss']
+  styleUrls: ['./search-result-item.component.scss'],
 })
 export class SearchResultItemComponent implements OnInit {
+  searchResult!: SearchResult;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchResult } from '../../types/SearchResult';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
+  searchResults!: Array<SearchResult>;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
