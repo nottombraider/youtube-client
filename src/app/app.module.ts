243650c5be69eda9f7d3aadd6dcccb15db6b0d62
nottombraider <larysa.yavorska@outlook.com>
2021-08-22 13:12:41 +0200

    feat: add filtering bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { LoginInfoComponent } from './components/login-info/login-info.component';
+import { FilteringBarComponent } from './components/filtering-bar/filtering-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchBarComponent, LoginInfoComponent],
+  declarations: [AppComponent, HeaderComponent, SearchBarComponent, LoginInfoComponent, FilteringBarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/filtering-bar/filtering-bar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilteringBarComponent } from './filtering-bar.component';
+
+describe('FilteringBarComponent', () => {
+  let component: FilteringBarComponent;
+  let fixture: ComponentFixture<FilteringBarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FilteringBarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FilteringBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
